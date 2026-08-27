1e516879ee368d54158ab9d16ea43b32690fe818
K8bus <K8bus@proton.me>
2026-08-27 21:11:24 +0200

    fix: resolve AbstractMethodError crash with Arts & Crafts decorated pots (bump to 1.0.0-beta.7.1)

--- src/main/java/com/ancient/patchup/block/arts_and_crafts/PatchUpDecoratedPotItemRenderer.java
+++ src/main/java/com/ancient/patchup/block/arts_and_crafts/PatchUpDecoratedPotItemRenderer.java
@@ -4,11 +4,10 @@ import com.kekecreations.arts_and_crafts.common.block.DyedDecoratedPotBlock;
 import com.kekecreations.arts_and_crafts.common.entity.DyedDecoratedPotBlockEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRenderer;
 import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
@@ -18,12 +17,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Environment(EnvType.CLIENT)
-public class PatchUpDecoratedPotItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
+public class PatchUpDecoratedPotItemRenderer implements BuiltinItemRenderer {
 
     private final Map<Block, DyedDecoratedPotBlockEntity> blockEntities = new HashMap<>();
 
     @Override
-    public void render(ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+    public void render(ItemStack stack, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         if (stack.getItem() instanceof BlockItem blockItem) {
             Block block = blockItem.getBlock();
             DyedDecoratedPotBlockEntity entity = blockEntities.computeIfAbsent(block, b -> {
